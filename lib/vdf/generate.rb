282d791d4e70695891be5e080263634e12eb051f
Phil <phil@sapphyrus.xyz>
2019-08-22 23:14:02 +0200

    Add generator

--- lib/vdf/generate.rb
+++ lib/vdf/generate.rb
@@ -1,10 +1,28 @@
 module VDF
 	class Generator
-		class << self
-			def generate(object)
-				object.inspect
-			end
-		end
+	  class << self
+
+	    def generate(object)
+	      generate_impl(object, 0)
+	    end
+
+	    private
+
+	    def generate_impl(object, level)
+	      result = ""
+	      indent = -"\t"*level
+
+	      object.each do |key, value|
+					if value.respond_to? :each
+						result << [indent, -'"', key, -"\"\n", indent, -"{\n", generate_impl(value, level + 1), indent, -"}\n"].join
+					else
+						result << [indent, -'"', key, -'"', indent, indent, -'"', value.to_s, -"\"\n"].join
+					end
+	      end
+
+	      result
+	    end
+	  end
 	end
 
 	def generate(object)
